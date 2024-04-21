3310832558a93ab689f49e245f33a16afa2b7ff7
kshksdrt <kaushik.sdrt@gmail.com>
2024-04-21 16:31:19 +0200

    Update a comment

--- init.lua
+++ init.lua
@@ -589,16 +589,19 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+
         -- clangd = {},
-        gopls = {},
+
+        gopls = {
+          cmd = { "gopls" },
+        },
+
         -- pyright = {},
+
         -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+
+        -- See https://github.com/pmizio/typescript-tools.nvim for more useful features
         tsserver = {
           setup = {
             init_options = {
@@ -621,7 +624,6 @@ require('lazy').setup({
             },
           },
         },
-
 
         lua_ls = {
           -- cmd = {...},

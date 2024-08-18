356a01d9428b5df955623dc1c5740d5f4ff0b71b
kshksdrt <kaushik.sdrt@gmail.com>
2024-08-18 22:56:25 +0200

    Add neorg

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -19,4 +19,10 @@ return {
             require("nvim-surround").setup()
         end
     },
+    {
+        "nvim-neorg/neorg",
+        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        config = true,
+    }
 }

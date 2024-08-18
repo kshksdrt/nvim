c3c36753bac5cd323f28e8a9fda1481377736bee
kshksdrt <kaushik.sdrt@gmail.com>
2024-08-18 19:56:29 +0200

    Configure nvim tree

--- init.lua
+++ init.lua
@@ -353,17 +353,38 @@ require('lazy').setup({
         },
         config = function()
           require("nvim-tree").setup {
+            actions = {
+              open_file = {
+                quit_on_open = true,
+              },
+            },
             sort = {
               sorter = "case_sensitive",
             },
             view = {
               width = 30,
               side = "right",
+              relativenumber = true,
             },
             renderer = {
               group_empty = true,
             },
+            filters = {
+              dotfiles = true,
+            }
           }
+
+          -- Keybinding to open Nvim-tree and reveal current file
+          vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+
+          -- Keybinding to close Nvim-tree with Escape key
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = "NvimTree",
+            callback = function()
+              vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>', ':lua require("nvim-tree.api").tree.close()<CR>',
+                { noremap = true, silent = true })
+            end
+          })
         end,
       }
     },

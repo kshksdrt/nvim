0ca6449ec09d88a0d78e925d176addd6ac716b58
kshksdrt <kaushik.sdrt@gmail.com>
2024-09-08 19:17:48 +0200

    Add keymaps to rename symbol and code actions similar to the visual studios

--- init.lua
+++ init.lua
@@ -682,11 +682,13 @@ require('lazy').setup({
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame Symbol')
+          map('<F2>', vim.lsp.buf.rename, 'Rename Symbol')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('<C-.>', vim.lsp.buf.code_action, 'Code Action')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.

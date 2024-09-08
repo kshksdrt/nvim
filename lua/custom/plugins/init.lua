50089b830391853bad28c4f6568fc6b7450500ac
kshksdrt <kaushik.sdrt@gmail.com>
2024-09-08 19:06:49 +0200

    Update cursor line color for better visibility

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,12 +14,7 @@ return {
 
             local c = require('vscode.colors').get_colors()
             require('vscode').setup({
-                -- Alternatively set style in setup
-                -- style = 'light'
-
-                -- Enable transparent background
                 transparent = true,
-
                 italic_comments = false,
 
                 -- Underline `@markup.link.*` variants
@@ -32,7 +27,7 @@ return {
                 group_overrides = {
                     -- this supports the same val table as vim.api.nvim_set_hl
                     -- use colors from this colorscheme by requiring vscode.colors!
-                    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+                    CursorLine = { bg = "#303030" },
                 }
             })
             require('vscode').load()

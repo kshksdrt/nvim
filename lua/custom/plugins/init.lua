8c4ccee415b0326f5719fc6d6d87b04013bf6310
kshksdrt <kaushik.sdrt@gmail.com>
2024-09-09 17:55:19 +0200

    Add git fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,90 +3,92 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-        'Mofiqul/vscode.nvim',
-        config = function()
-            -- Lua:
-            -- For dark theme (neovim's default)
-            vim.o.background = 'dark'
-            -- For light theme
-            -- vim.o.background = 'light'
+  {
+    'Mofiqul/vscode.nvim',
+    config = function()
+      -- Lua:
+      -- For dark theme (neovim's default)
+      vim.o.background = 'dark'
+      -- For light theme
+      -- vim.o.background = 'light'
 
-            local c = require('vscode.colors').get_colors()
-            require('vscode').setup({
-                transparent = true,
-                italic_comments = false,
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup {
+        transparent = true,
+        italic_comments = false,
 
-                -- Underline `@markup.link.*` variants
-                underline_links = true,
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
 
-                -- Disable nvim-tree background color
-                disable_nvimtree_bg = true,
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
 
-                -- Override highlight groups (see ./lua/vscode/theme.lua)
-                group_overrides = {
-                    -- this supports the same val table as vim.api.nvim_set_hl
-                    -- use colors from this colorscheme by requiring vscode.colors!
-                    CursorLine = { bg = "#303030" },
-                }
-            })
-            require('vscode').load()
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          CursorLine = { bg = '#303030' },
+        },
+      }
+      require('vscode').load()
 
-            -- load the theme without affecting devicon colors.
-            vim.cmd.colorscheme "vscode"
-        end
-    },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            local bufferline = require('bufferline');
-            bufferline.setup({
-                options = {
-                    style_preset = bufferline.style_preset.no_italic,
-                    indicator = {
-                        style = 'none',
-                    },
-                    separator_style = {}
-                }
-            })
-        end
-    },
-    {
-        'ggandor/leap.nvim',
-        version = "*",
-        config = function()
-            vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lj', '<Plug>(leap-forward-to)',
-                { noremap = true, silent = true, desc = "Leap forward" })
-            vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lk', '<Plug>(leap-backward-to)',
-                { noremap = true, silent = true, desc = "Leap backward" })
-        end
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup()
-        end
-    },
-    {
-        "petertriho/nvim-scrollbar",
-        version = "*",
-        config = function()
-            require("scrollbar").setup({
-                handle = {
-                    text = "  ",
-                    blend = 0,
-                    color = "#404040"
-                },
-            })
-        end
-    },
-    {
-        "yutkat/confirm-quit.nvim",
-        event = "CmdlineEnter",
-        opts = {},
-    }
+      -- load the theme without affecting devicon colors.
+      vim.cmd.colorscheme 'vscode'
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      local bufferline = require 'bufferline'
+      bufferline.setup {
+        options = {
+          style_preset = bufferline.style_preset.no_italic,
+          indicator = {
+            style = 'none',
+          },
+          separator_style = {},
+        },
+      }
+    end,
+  },
+  {
+    'ggandor/leap.nvim',
+    version = '*',
+    config = function()
+      vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lj', '<Plug>(leap-forward-to)', { noremap = true, silent = true, desc = 'Leap forward' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '<leader>lk', '<Plug>(leap-backward-to)', { noremap = true, silent = true, desc = 'Leap backward' })
+    end,
+  },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup()
+    end,
+  },
+  {
+    'petertriho/nvim-scrollbar',
+    version = '*',
+    config = function()
+      require('scrollbar').setup {
+        handle = {
+          text = '  ',
+          blend = 0,
+          color = '#404040',
+        },
+      }
+    end,
+  },
+  {
+    'tpope/vim-fugitive',
+    version = '*',
+  },
+  {
+    'yutkat/confirm-quit.nvim',
+    event = 'CmdlineEnter',
+    opts = {},
+  },
 }

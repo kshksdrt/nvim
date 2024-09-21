8214494985b50577d0034b6fdf9c24b42bb164c0
kshksdrt <kaushik.sdrt@gmail.com>
2024-09-21 23:44:26 +0200

    Improve neo-tree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -3,7 +3,7 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  branch = 'v3.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -11,16 +11,38 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>f', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        position = 'right',
-        mappings = {
-          ['\\'] = 'close_window',
+  config = function()
+    vim.cmd [[highlight NeoTreeIndentMarker guifg=#3f3f3f]]
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      filesystem = {
+        window = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            visible = true,
+          },
+          follow_current_file = {
+            enabled = true,
+          },
+          position = 'right',
+          mappings = {
+            ['<esc>'] = 'close_window', -- Close the tree with Escape key
+            ['\\'] = 'close_window',
+          },
         },
       },
-    },
-  },
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            -- auto close
+            require('neo-tree').close_all()
+          end,
+        },
+      },
+    }
+  end,
 }

11cec8e617c7208faca366655a3e3fc5a3e71734
kshksdrt <kaushik.sdrt@gmail.com>
2024-10-08 23:24:53 +0200

    Remove commented code

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -226,47 +226,4 @@ return {
       }
     end,
   },
-  -- { -- Adds a file tree to Neovim
-  --   'nvim-tree/nvim-tree.lua',
-  --   version = '*',
-  --   lazy = false,
-  --   dependencies = {
-  --     'nvim-tree/nvim-web-devicons',
-  --   },
-  --   config = function()
-  --     require('nvim-tree').setup {
-  --       actions = {
-  --         open_file = {
-  --           quit_on_open = true,
-  --         },
-  --       },
-  --       sort = {
-  --         sorter = 'case_sensitive',
-  --       },
-  --       view = {
-  --         width = 30,
-  --         side = 'right',
-  --         relativenumber = true,
-  --         adaptive_size = true,
-  --       },
-  --       renderer = {
-  --         group_empty = true,
-  --       },
-  --       filters = {
-  --         dotfiles = false,
-  --       },
-  --     }
-  --
-  --     -- Keybinding to open Nvim-tree and reveal current file
-  --     vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
-  --
-  --     -- Keybinding to close Nvim-tree with Escape key
-  --     vim.api.nvim_create_autocmd('FileType', {
-  --       pattern = 'NvimTree',
-  --       callback = function()
-  --         vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>', ':lua require("nvim-tree.api").tree.close()<CR>', { noremap = true, silent = true })
-  --       end,
-  --     })
-  --   end,
-  -- },
 }

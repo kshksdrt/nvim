73d58ccddc4666b7faa1470a6a18fb324c0a0421
kshksdrt <kaushik.sdrt@gmail.com>
2024-10-28 09:56:40 +0100

    Remove a plugin

--- init.lua
+++ init.lua
@@ -586,7 +586,7 @@ require('lazy').setup({
 
       vim.keymap.set('n', '<leader>sh', theme_wrapper(builtin.help_tags), { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', theme_wrapper(builtin.keymaps), { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', theme_wrapper(telescope.extensions.smart_open.smart_open), { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', theme_wrapper(builtin.find_files), { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', theme_wrapper(builtin.builtin), { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', theme_wrapper(builtin.grep_string), { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', theme_wrapper(builtin.live_grep), { desc = '[S]earch by [G]rep' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -204,21 +204,6 @@ return {
     'MattesGroeger/vim-bookmarks',
     config = function() end,
   },
-  -- Smart open_buffer_menu
-  {
-    'danielfalk/smart-open.nvim',
-    branch = '0.2.x',
-    config = function()
-      require('telescope').load_extension 'smart_open'
-    end,
-    dependencies = {
-      'kkharji/sqlite.lua',
-      -- Only required if using match_algorithm fzf
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-      { 'nvim-telescope/telescope-fzy-native.nvim' },
-    },
-  },
   -- Adds a better quickfix list UI
   {
     'kevinhwang91/nvim-bqf',

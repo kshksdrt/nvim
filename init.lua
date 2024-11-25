83bdd965c82c141c7c6e291d81e5aab9ddea5b5a
kshksdrt <kaushik.sdrt@gmail.com>
2024-11-25 11:35:30 +0100

    Formatting

--- init.lua
+++ init.lua
@@ -840,12 +840,12 @@ require('lazy').setup({
 
       -- Change diagnostic symbols in the sign column (gutter)
       if vim.g.have_nerd_font then
-      local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      local diagnostic_signs = {}
-      for type, icon in pairs(signs) do
-      diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      end
-      vim.diagnostic.config { signs = { text = diagnostic_signs } }
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
       end
 
       -- LSP servers and clients are able to communicate to each other what features they support.

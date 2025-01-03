7c2ea8bc7a497d86495b1b97106724c33c55daf7
kshksdrt <kaushik.sdrt@gmail.com>
2025-01-03 22:25:39 +0100

    Update which-key styles

--- init.lua
+++ init.lua
@@ -345,9 +345,11 @@ require('lazy').setup({
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
       delay = 0,
+      preset = 'helix',
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
+        separator = '',
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {

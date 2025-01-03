435aebdcfae249f87eef42db35e794c7b1070d31
kshksdrt <kaushik.sdrt@gmail.com>
2025-01-03 20:59:55 +0100

    Add plugin to enhance quickfix list

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -158,6 +158,47 @@ return {
   },
   -- Adds a better quickfix list UI
   {
+    'stevearc/quicker.nvim',
+    event = 'FileType qf',
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+    config = function()
+      local quicker = require 'quicker'
+
+      vim.keymap.set('n', '<leader>q', function()
+        quicker.toggle()
+      end, {
+        desc = 'Toggle quickfix',
+      })
+
+      vim.keymap.set('n', '<leader>l', function()
+        quicker.toggle { loclist = true }
+      end, {
+        desc = 'Toggle loclist',
+      })
+
+      quicker.setup {
+        keys = {
+          {
+            '>',
+            function()
+              quicker.expand { before = 2, after = 2, add_to_existing = true }
+            end,
+            desc = 'Expand quickfix context',
+          },
+          {
+            '<',
+            function()
+              quicker.collapse()
+            end,
+            desc = 'Collapse quickfix context',
+          },
+        },
+      }
+    end,
+  },
+  {
     'kevinhwang91/nvim-bqf',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',

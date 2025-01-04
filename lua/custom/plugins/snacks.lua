64c1047ed9dbe0a6dc24755e06912dabcdbc5db0
kshksdrt <kaushik.sdrt@gmail.com>
2025-01-04 15:20:07 +0100

    Improve snacks setup

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,4 +1,21 @@
 return {
   'folke/snacks.nvim',
   lazy = false,
+  priority = 1000,
+  ---@type snacks.Config
+  opts = {
+    scroll = {
+      enabled = false,
+      animate = {
+        duration = { step = 15, total = 250 },
+        easing = 'inCubic',
+        fps = 60,
+      },
+      spamming = 10, -- threshold for spamming detection
+      -- what buffers to animate
+      filter = function(buf)
+        return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and vim.bo[buf].buftype ~= 'terminal'
+      end,
+    },
+  },
 }

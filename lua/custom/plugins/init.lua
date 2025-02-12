58247507bf0c5bf89aee8b4cf4eecb029ef91c09
kshksdrt <kaushik.sdrt@gmail.com>
2025-02-13 00:33:07 +0100

    Remove noice.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,18 +8,6 @@ return {
     'stevearc/dressing.nvim',
     opts = {},
   },
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      cmdline = {
-        enabled = true,
-      },
-    },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-    },
-  },
   -- Git wrapper
   {
     'tpope/vim-fugitive',

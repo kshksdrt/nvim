1b95a4ec6c41a25dbc6ecffb5e7ff838bd7cb330
kshksdrt <kaushik.sdrt@gmail.com>
2025-02-13 00:09:29 +0100

    Refactor neo-tree and nvim-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -39,57 +39,8 @@ return {
       pattern = 'NvimTree',
       callback = function()
         vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>', ':lua require("nvim-tree.api").tree.close()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(0, 'n', '\\', ':lua require("nvim-tree.api").tree.close()<CR>', { noremap = true, silent = true })
       end,
     })
   end,
 }
-
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   branch = 'v3.x',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   cmd = 'Neotree',
---   keys = {
---     { '<leader>f', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
---   },
---   config = function()
---     vim.cmd [[highlight NeoTreeIndentMarker guifg=#3f3f3f]]
---     require('neo-tree').setup {
---       close_if_last_window = true,
---       filesystem = {
---         use_linenumbers = true,
---         window = {
---           filtered_items = {
---             hide_dotfiles = false,
---             hide_gitignored = false,
---             visible = true,
---           },
---           follow_current_file = {
---             enabled = true,
---           },
---           position = 'right',
---           mappings = {
---             ['<esc>'] = 'close_window',
---             ['<leader>f'] = 'close_window',
---           },
---         },
---       },
---       event_handlers = {
---         {
---           event = 'file_opened',
---           handler = function()
---             -- auto close
---             require('neo-tree').close_all()
---           end,
---         },
---       },
---     }
---   end,
--- }

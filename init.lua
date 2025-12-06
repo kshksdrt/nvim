6fd5f9cbbc743f66c4d50ac94e1509672fa3b6e7
kshksdrt <kaushik.sdrt@gmail.com>
2025-12-06 15:03:39 +0100

    Organize code comments

--- init.lua
+++ init.lua
@@ -643,34 +643,26 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       {
         'saghen/blink.cmp',
-        lazy = false, -- lazy loading handled internally
-        -- optional: provides snippets for the snippet source
+        lazy = false,
         dependencies = {
-          'rafamadriz/friendly-snippets',
+          'rafamadriz/friendly-snippets', -- optional: provides snippets for the snippet source
           'xzbdmw/colorful-menu.nvim',
         },
-
-        -- use a release tag to download pre-built binaries
-        version = 'v0.*',
-        -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
-          -- 'default' for mappings similar to built-in completion
-          -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-          -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-          -- see the "default configuration" section below for full documentation on how to define
-          -- your own keymap.
           keymap = {
+            -- 'default' for mappings similar to built-in completion
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            -- see the "default configuration" section below for full documentation on how to define
+            -- your own keymap.
             preset = 'default',
           },
 
           completion = {
             menu = {
+              auto_show = true,
               draw = {
                 -- We don't need label_description now because label and label_description are already
                 -- combined together in label by colorful-menu.nvim.
@@ -710,7 +702,9 @@ require('lazy').setup({
         },
         -- allows extending the enabled_providers array elsewhere in your config
         -- without having to redefining it
-        opts_extend = { 'sources.completion.enabled_providers' },
+        opts_extend = {
+          'sources.completion.enabled_providers',
+        },
       },
 
       -- Useful status updates for LSP.

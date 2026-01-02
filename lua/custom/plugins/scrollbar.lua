64205dc19cbfe2a9c06193554d548c6dd0b8f852
kshksdrt <kaushik.sdrt@gmail.com>
2026-01-02 02:14:03 +0100

    Update scrollbar plugin

--- lua/custom/plugins/scrollbar.lua
+++ lua/custom/plugins/scrollbar.lua
@@ -1,50 +1,25 @@
 return {
-  'petertriho/nvim-scrollbar',
+  'lewis6991/satellite.nvim',
   version = '*',
   config = function()
-    require('scrollbar').setup {
-      handle = {
-        text = ' ',
-        blend = 0,
-        color = '#404040',
-      },
-      handlers = {
-        cursor = true,
-        diagnostic = true,
-        gitsigns = true, -- Requires gitsigns
-        handle = true,
-        search = false, -- Requires hlslens
-        ale = false, -- Requires ALE
-      },
-      marks = {
-        GitAdd = {
-          text = '│',
-          priority = 7,
-          gui = nil,
-          color = nil,
-          cterm = nil,
-          color_nr = nil, -- cterm
-          highlight = 'GitSignsAdd',
-        },
-        GitChange = {
-          text = '│',
-          priority = 7,
-          gui = nil,
-          color = nil,
-          cterm = nil,
-          color_nr = nil, -- cterm
-          highlight = 'GitSignsChange',
-        },
-        GitDelete = {
-          text = '│',
-          priority = 7,
-          gui = nil,
-          color = nil,
-          cterm = nil,
-          color_nr = nil, -- cterm
-          highlight = 'GitSignsDelete',
-        },
-      },
+    -- Create an autocommand to force the highlight color and immediately trigger the autocommand.
+    -- This runs every time you load a colorscheme to prevent it from being cleared.
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      pattern = '*',
+      callback = function()
+        vim.api.nvim_set_hl(0, 'ScrollView', {
+          fg = '#424242',
+          bg = '#d3d3d3',
+          force = true,
+        })
+      end,
+    })
+    vim.api.nvim_exec_autocmds('ColorScheme', {})
+
+    require('satellite').setup {
+      current_only = false,
+      winblend = 50,
+      zindex = 40,
       excluded_filetypes = {
         'cmp_docs',
         'cmp_menu',
@@ -52,7 +27,97 @@ return {
         'prompt',
         'TelescopePrompt',
         'snacks_input',
+        'alpha',
+        'dashboard',
+      },
+      width = 2,
+      handlers = {
+        cursor = {
+          enable = true,
+          symbols = { '⎺', '⎻', '⎼', '⎽' },
+        },
+        search = { enable = true },
+        diagnostic = {
+          enable = true,
+          signs = { '-', '=', '≡' },
+          min_severity = vim.diagnostic.severity.HINT,
+        },
+        gitsigns = {
+          enable = true,
+          signs = {
+            add = '│',
+            change = '│',
+            delete = '-',
+          },
+        },
+        marks = {
+          enable = true,
+          show_builtins = false,
+          key = 'm',
+        },
+        quickfix = {
+          signs = { '-', '=', '≡' },
+        },
       },
     }
   end,
 }
+
+-- return {
+--   'petertriho/nvim-scrollbar',
+--   version = '*',
+--   config = function()
+--     require('scrollbar').setup {
+--       handle = {
+--         text = ' ',
+--         blend = 0,
+--         color = '#404040',
+--       },
+--       handlers = {
+--         cursor = true,
+--         diagnostic = true,
+--         gitsigns = true, -- Requires gitsigns
+--         handle = true,
+--         search = false, -- Requires hlslens
+--         ale = false, -- Requires ALE
+--       },
+--       marks = {
+--         GitAdd = {
+--           text = '│',
+--           priority = 7,
+--           gui = nil,
+--           color = nil,
+--           cterm = nil,
+--           color_nr = nil, -- cterm
+--           highlight = 'GitSignsAdd',
+--         },
+--         GitChange = {
+--           text = '│',
+--           priority = 7,
+--           gui = nil,
+--           color = nil,
+--           cterm = nil,
+--           color_nr = nil, -- cterm
+--           highlight = 'GitSignsChange',
+--         },
+--         GitDelete = {
+--           text = '│',
+--           priority = 7,
+--           gui = nil,
+--           color = nil,
+--           cterm = nil,
+--           color_nr = nil, -- cterm
+--           highlight = 'GitSignsDelete',
+--         },
+--       },
+--       excluded_filetypes = {
+--         'cmp_docs',
+--         'cmp_menu',
+--         'noice',
+--         'prompt',
+--         'TelescopePrompt',
+--         'snacks_input',
+--       },
+--     }
+--   end,
+-- }

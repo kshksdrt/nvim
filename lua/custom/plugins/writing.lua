e2073aefab1b83aa78acf2bf9999d9850b41b2f9
kshksdrt <kaushik.sdrt@gmail.com>
2026-05-27 19:58:06 +0200

    Add pandoc.nvim

--- lua/custom/plugins/writing.lua
+++ lua/custom/plugins/writing.lua
@@ -55,4 +55,74 @@ return {
       map('<leader>mr', cm.remove_all_metadata, '[R]emove all metadata')
     end,
   },
+  -- Viewing
+  {
+    'aspeddro/pandoc.nvim',
+    config = function()
+      require('pandoc').setup()
+
+      vim.api.nvim_create_user_command('PandocPreview', function()
+        if vim.bo.filetype ~= 'markdown' then
+          vim.notify('PandocPreview only works for markdown files', vim.log.levels.WARN)
+          return
+        end
+
+        local config = require('pandoc.config').get()
+        local bin = config.default.bin or 'pandoc'
+        local input = vim.api.nvim_buf_get_name(0)
+        if input == '' then
+          vim.notify('Buffer must be saved to a file first', vim.log.levels.ERROR)
+          return
+        end
+        local output = vim.fn.tempname() .. '.html'
+
+        -- Save buffer before rendering
+        vim.cmd 'update'
+
+        vim.fn.jobstart({ bin, input, '--standalone', '--output', output }, {
+          on_exit = function(_, code)
+            if code == 0 then
+              vim.notify('Pandoc: HTML render complete. Opening...', vim.log.levels.INFO)
+              vim.ui.open(output)
+            else
+              vim.notify('Pandoc: Rendering failed', vim.log.levels.ERROR)
+            end
+          end,
+        })
+      end, { desc = 'Render markdown to HTML and open in browser' })
+
+      vim.api.nvim_create_user_command('PandocToDoc', function()
+        if vim.bo.filetype ~= 'markdown' then
+          vim.notify('PandocToDoc only works for markdown files', vim.log.levels.WARN)
+          return
+        end
+
+        local input = vim.api.nvim_buf_get_name(0)
+        if input == '' then
+          vim.notify('Buffer must be saved to a file first', vim.log.levels.ERROR)
+          return
+        end
+
+        local filename = vim.fn.fnamemodify(input, ':t:r')
+        local output_dir = vim.fn.expand '~/Documents/'
+        local output = output_dir .. filename .. '.html'
+
+        -- Save buffer before rendering
+        vim.cmd 'update'
+
+        local config = require('pandoc.config').get()
+        local bin = config.default.bin or 'pandoc'
+
+        vim.fn.jobstart({ bin, input, '--standalone', '--output', output }, {
+          on_exit = function(_, code)
+            if code == 0 then
+              vim.notify('Pandoc: Exported to ' .. output, vim.log.levels.INFO)
+            else
+              vim.notify('Pandoc: Export failed', vim.log.levels.ERROR)
+            end
+          end,
+        })
+      end, { desc = 'Export markdown to ~/Documents/ as HTML' })
+    end,
+  },
 }

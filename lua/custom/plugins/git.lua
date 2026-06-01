9f6317c7f5cf7d3e707f5453c997ee6d43e06def
kshksdrt <kaushik.sdrt@gmail.com>
2026-06-01 09:42:57 +0200

    Update diffview config

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,6 +1,22 @@
 return {
-  'sindrets/diffview.nvim',
+  'dlyongemallo/diffview.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  config = function()
+    require('diffview').setup {
+      enhanced_diff_hl = true,
+      use_icons = true,
+      view = {
+        default = { layout = 'diff1_inline' },
+        merge_tool = { layout = 'diff3_horizontal' },
+      },
+      file_panel = {
+        listing_style = 'tree',
+        win_config = { position = 'left', width = 35 }, -- Use "auto" to fit content
+      },
+      hooks = {}, -- See :h diffview-config-hooks
+      keymaps = {}, -- See :h diffview-config-keymaps
+    }
+  end,
 }

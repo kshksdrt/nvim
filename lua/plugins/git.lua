e0696854cba4b698613ff5fcbd0f1cd45489e7d7
kshksdrt <kaushik.sdrt@gmail.com>
2026-06-30 17:23:37 +0200

    Add ability to better cycle through changes in git while reviewing using CodeDiff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,9 @@
 return {
   {
     'esmuellert/codediff.nvim',
-    cmd = 'CodeDiff',
+    -- The Cycling* commands are listed so lazy loads the plugin (running config
+    -- below) before dispatching to the real commands it defines.
+    cmd = { 'CodeDiff', 'CodeDiffCyclingReset', 'CodeDiffCyclingEnable' },
     opts = {
       keymaps = {
         view = {
@@ -29,13 +31,39 @@ return {
           },
         },
       },
+      diff = {
+        cycle_next_hunk = false,
+        cycle_next_file = false,
+        cycle_hunks_across_files = false,
+      },
     },
+    config = function(_, opts)
+      require('codediff').setup(opts)
+
+      local cycling_keys = { 'cycle_next_hunk', 'cycle_next_file', 'cycle_hunks_across_files' }
+      local set_cycling = function(value)
+        local diff_opts = require('codediff.config').options.diff
+        for _, key in ipairs(cycling_keys) do
+          if value == nil then
+            diff_opts[key] = opts.diff[key]
+          else
+            diff_opts[key] = value
+          end
+        end
+      end
+
+      vim.api.nvim_create_user_command('CodeDiffCyclingReset', function()
+        set_cycling(nil)
+      end, { desc = 'Restore CodeDiff hunk/file cycling to configured defaults' })
+      vim.api.nvim_create_user_command('CodeDiffCyclingEnable', function()
+        set_cycling(true)
+      end, { desc = 'Enable all CodeDiff hunk/file cycling' })
+    end,
   },
   {
 
     'dlyongemallo/diffview.nvim',
     dependencies = {
-
       'nvim-lua/plenary.nvim',
     },
     config = function()

36dc864de64530e68203cf8359e45e57410d519d
kshksdrt <kaushik.sdrt@gmail.com>
2026-07-23 20:42:55 +0200

    Improve incremental selection config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,15 +14,6 @@ return {
           additional_vim_regex_highlighting = true,
         },
         indent = { enable = true, disable = { 'ruby' } },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = 'gnn',
-            node_incremental = '<PageUp>',
-            scope_incremental = 'grc',
-            node_decremental = '<PageDown>',
-          },
-        },
       }
       require('nvim-treesitter.install').prefer_git = false
       require('nvim-treesitter.install').compilers = { 'zig' }
@@ -72,6 +63,88 @@ return {
 
       vim.opt.foldmethod = 'manual'
       vim.opt.foldenable = false
+
+      -- Node nav + incremental selection, hand-rolled since `main` dropped the `incremental_selection` module.
+      local selection_stack = {}
+
+      local function ranges_equal(a, b)
+        local a1, a2, a3, a4 = a:range()
+        local b1, b2, b3, b4 = b:range()
+        return a1 == b1 and a2 == b2 and a3 == b3 and a4 == b4
+      end
+
+      local function select_node(buf, node)
+        local srow, scol, erow, ecol = node:range()
+        srow, scol, erow = srow + 1, scol + 1, erow + 1
+        if ecol == 0 then -- exclusive end col; 0 ⇒ node ends at line start, back up one line
+          erow = erow - 1
+          local line = vim.api.nvim_buf_get_lines(buf, erow - 1, erow, false)[1] or ''
+          ecol = math.max(#line, 1)
+        end
+        if vim.fn.mode():match('^[vV\022]') then
+          vim.cmd('normal! \27')
+        end
+        vim.fn.setpos('.', { 0, srow, scol, 0 })
+        vim.cmd('normal! v')
+        vim.fn.setpos('.', { 0, erow, ecol, 0 })
+      end
+
+      local function start_selection(buf)
+        local node = vim.treesitter.get_node()
+        if not node then return nil end
+        selection_stack[buf] = { node }
+        return node
+      end
+
+      local function ts_selection(grow)
+        local buf = vim.api.nvim_get_current_buf()
+        local in_visual = vim.fn.mode():match('^[vV\022]') ~= nil
+        local stack = selection_stack[buf]
+        local node
+        if not in_visual or not stack or #stack == 0 then
+          node = start_selection(buf)
+          if not node then return end
+        elseif grow then
+          local top = stack[#stack]
+          local parent = top:parent()
+          while parent and ranges_equal(parent, top) do
+            parent = parent:parent() -- skip same-range ancestors
+          end
+          if parent then table.insert(stack, parent) end
+          node = stack[#stack]
+        else
+          if #stack > 1 then table.remove(stack) end
+          node = stack[#stack]
+        end
+        select_node(buf, node)
+      end
+
+      local function ts_init_selection()
+        local buf = vim.api.nvim_get_current_buf()
+        local node = start_selection(buf)
+        if node then select_node(buf, node) end
+      end
+
+      local function goto_parent_node()
+        local node = vim.treesitter.get_node()
+        if not node then return end
+        local crow, ccol = unpack(vim.api.nvim_win_get_cursor(0))
+        crow = crow - 1
+        local parent = node:parent()
+        while parent do
+          local prow, pcol = parent:start()
+          if prow ~= crow or pcol ~= ccol then
+            vim.api.nvim_win_set_cursor(0, { prow + 1, pcol })
+            return
+          end
+          parent = parent:parent()
+        end
+      end
+
+      vim.keymap.set('n', '<leader>^', goto_parent_node, { desc = 'Treesitter: go to parent node' })
+      vim.keymap.set('n', 'gnn', ts_init_selection, { desc = 'Treesitter: start incremental selection' })
+      vim.keymap.set('x', 'H', function() ts_selection(true) end, { desc = 'Treesitter: grow selection to parent node' }) -- visual-only; normal H/L = tab nav
+      vim.keymap.set('x', 'L', function() ts_selection(false) end, { desc = 'Treesitter: shrink selection to child node' })
     end,
   },
 }
